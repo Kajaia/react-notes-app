eb57e403678b030231379369e56393789f655be1
Kajaia <lashakajaia31@gmail.com>
2022-10-15 23:47:01 +0400

    Removed bootstrap and fontawesome, instead added them from cdn

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import "bootstrap/dist/css/bootstrap.css";
 import "./assets/style.css";
-import "@fortawesome/fontawesome-free/js/all.js";
-import "bootstrap/dist/js/bootstrap.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
